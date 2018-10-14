e9e5ee725b978856eb1f215832967583bc19a7d3
Jessica <jesslui1008@gmail.com>
2018-10-14 00:10:37 -0400

    animation

--- src/states/Play.java
+++ src/states/Play.java
@@ -10,11 +10,19 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 import core.Game;
 
 public class Play extends BasicGameState {
 
+	int pointcount=0;
+	boolean b1, b2, b3, b4 = false;
+	int t1, t2, t3, t4 = 0;
+	
+	
 	private static final org.newdawn.slick.Font Verdana = null;
 	
 	int[] data;
@@ -25,6 +33,7 @@ public class Play extends BasicGameState {
 	
 	//y coord of top of the hit circles
 	int hitY = 706;
+	int ahitY = hitY-42;
 	
 	//pixel difference between array elements
 	int indexGap = 15;
@@ -35,10 +44,17 @@ public class Play extends BasicGameState {
 	int x2 = 730;
 	int x3 = 994;
 	int x4 = 1259;
+	int ax1 = x1-42;
+	int ax2 = x2-42;
+	int ax3 = x3-42;
+	int ax4 = x4-42;
 	int iconSize = 222;
+	int aiconSize = iconSize*138/100;
 	int score = 0;
 	Image bg, i1, i2, i3, i4, back, tap;
 	String songname = "SONG NAME";
+	Image a1, a2, a3, a4;
+	
 
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
@@ -50,6 +66,11 @@ public class Play extends BasicGameState {
 		tap = new Image("res/images/1double-tap.png");
 		back = new Image("res/images/back.png");
 
+		a1 = new Image("res/images/2glowmake-fist.png");
+		a2 = new Image("res/images/3glowspread-fingers.png");
+		a3 = new Image("res/images/4glowwave-left.png");
+		a4 = new Image("res/images/5glowwave-right.png");
+		
 		// make sure to derive the size
 		try {
 			data = beats.getarray("http://ericamwang.com/videoplayback.aif");
@@ -80,15 +101,34 @@ public class Play extends BasicGameState {
 
 		g.setFont(Game.title);
 		g.drawString("NOW PLAYING:", 1380, 120);
-
 		g.setFont(Game.text);
 		g.drawString(songname, 1380, 190);
-
-		g.drawImage(i1, x1, hitY, x1 + iconSize, hitY + iconSize, 0, 0, i1.getWidth(), i1.getHeight());
-		g.drawImage(i2, x2, hitY, x2 + iconSize, hitY + iconSize, 0, 0, i2.getWidth(), i2.getHeight());
-		g.drawImage(i3, x3, hitY, x3 + iconSize, hitY + iconSize, 0, 0, i3.getWidth(), i3.getHeight());
-		g.drawImage(i4, x4, hitY, x4 + iconSize, hitY + iconSize, 0, 0, i4.getWidth(), i4.getHeight());
-
+		
+		if(!b1) {
+			g.drawImage(i1, x1, hitY, x1 + iconSize, hitY + iconSize, 0, 0, i1.getWidth(), i1.getHeight());			
+		}
+		else {
+        	g.drawImage(a1, ax1, ahitY, ax1 + aiconSize, ahitY + aiconSize, 0, 0, a1.getWidth(), a1.getHeight());
+		}
+		
+		if(!b2) {
+			g.drawImage(i2, x2, hitY, x2 + iconSize, hitY + iconSize, 0, 0, i2.getWidth(), i2.getHeight());
+		} else {
+        	g.drawImage(a2, ax2, ahitY, ax2 + aiconSize, ahitY + aiconSize, 0, 0, a2.getWidth(), a2.getHeight());
+		}
+		
+		if(!b3) {
+			g.drawImage(i3, x3, hitY, x3 + iconSize, hitY + iconSize, 0, 0, i3.getWidth(), i3.getHeight());
+		} else {
+        	g.drawImage(a3, ax3, ahitY, ax3 + aiconSize, ahitY + aiconSize, 0, 0, a3.getWidth(), a3.getHeight());
+		}
+		
+		if(!b4) {
+			g.drawImage(i4, x4, hitY, x4 + iconSize, hitY + iconSize, 0, 0, i4.getWidth(), i4.getHeight());			
+		} else {
+        	g.drawImage(a4, ax4, ahitY, ax4 + aiconSize, ahitY + aiconSize, 0, 0, a4.getWidth(), a4.getHeight());
+		}
+		
 		for (int i = 0; i < data.length; i++) {
 
 			int x = 0;
@@ -132,6 +172,48 @@ public class Play extends BasicGameState {
 		for (int i = 0; i < circleY.length; i++) {
 			
 			circleY[i] += distance;
+		}
+		
+		
+		if(container.getInput().isKeyDown(Input.KEY_Y)) { //change code
+			b1 = true;
+			t1 = 1000;
+		}	
+		if(t1 > 0)	{
+			t1 -= delta;
+		} else {
+			b1 = false;
+		}
+		
+		
+		if(container.getInput().isKeyDown(Input.KEY_U)) { //change code
+			b2 = true;
+			t2 = 1000;
+		}
+		if(t2 > 0)	{
+			t2 -= delta;
+		} else {
+			b2 = false;
+		}
+		
+		if(container.getInput().isKeyDown(Input.KEY_I)) { //change code
+			b3 = true;
+			t3 = 1000;
+		}
+		if(t3 > 0)	{
+			t3 -= delta;
+		} else {
+			b3 = false;
+		}
+		
+		if(container.getInput().isKeyDown(Input.KEY_O)) { //change code
+			b4 = true;
+			t4 = 1000;
+		}
+		if(t4 > 0)	{
+			t4 -= delta;
+		} else {
+			b4 = false;
 		}
 	}
 
